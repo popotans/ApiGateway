7e9357d0a477aff703dd8f7cbd9d7315b17000b0
vding <vding2010@live.com>
2016-09-05 21:45:24 +0800

    add

---
+++ src/ApiGateway/Controllers/GatewayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiGateway.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    public class GatewayController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        [Route("gateway/{Method}")]
+        public void Post(ApiRequestModel model)
+        {
+
+        }
+    }
+}
---
+++ src/ApiGateway/Models/ApiRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ApiGateway.Models
+{
+    public class ApiRequestModel
+    {
+        public string AccessKey { get; set; }
+        public string ApiName { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Body { get; set; }
+        public string Sign { get; set; }
+    }
+}
