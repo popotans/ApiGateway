cfd6918d2e563baf6591a5cf8022670bfc811dc6
vding <vding2010@live.com>
2016-09-05 21:26:41 +0800

    新增

---
+++ src/ApiGateway/Filters/IFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Filters
+{
+    public interface IFilter
+    {
+        string FilterType { get; }
+        int FilterOrder { get; }
+        void Execute();
+    }
+}
